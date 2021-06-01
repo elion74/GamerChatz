08d70ec7590e9bf8d6eba23099b97631681c12ed
Elion Shahini <elion.shahini74@gmail.com>
2021-06-01 18:48:06 +0200

    Group component created

--- source/client/src/components/Sidebar/Sidebar.js
+++ source/client/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Chat from '../Chat/Chat.js';
+import Group from '../Group/Group.js';
 
 import './Sidebar.css';
 
@@ -10,15 +11,20 @@ export default function Sidebar(){
 return(
     <aside className ='sidebar'>
         <section className = 'yourchats_sec'>
-            <h2>Your chats</h2>
+            <h2 className ='yourchats_headline'>Your chats</h2>
             <Chat />
-            {/* chat component */}
+            <Chat />
+            <Chat />
+            <Chat />
         </section>
 
-        {/* <section className ='yourgroups_sec'>
-            <h2>Your groups</h2>
-            group component or chat component
-        </section> */}
+        <section className ='yourgroups_sec'>
+            <h2 className = 'yourgroups_headline'>Your groups</h2>
+            <Group />
+            <Group />
+            <Group />
+            {/* group component or chat component */}
+        </section>
 
         <section>
             
