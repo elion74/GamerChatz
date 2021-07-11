74ab1614b3e0b492c8cfd14729c52b6bad1a34a2
Elion Shahini <elion.shahini74@gmail.com>
2021-07-11 17:32:48 +0200

    Textbar styles completed

--- source/client/src/components/TextBar/TextBar.js
+++ source/client/src/components/TextBar/TextBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import './TextBar.css';
+import sendbutton from '../../assets/sendbutton.png';
 
 export default function TextBar() {
 
     return (
         <section className='textbar_sec'>
-            <div className='text_bar'>
-                asf
-            </div>
+            <input type = 'text' className = 'text_bar'/>
+            <button className = 'textbar_button'> <img className = 'textbarbutton_image' alt = 'sendbutton' src = {sendbutton}/></button>
         </section>
 
     )
