8e145ebea18da0af60fdb1167e43ae7cc7252bb2
Elion Shahini <elion.shahini74@gmail.com>
2021-07-12 15:16:31 +0200

    added text cover in chats page so that the previous text is not visible

--- source/client/src/components/TextBar/TextBar.js
+++ source/client/src/components/TextBar/TextBar.js
@@ -6,10 +6,15 @@ import sendbutton from '../../assets/sendbutton.png';
 export default function TextBar() {
 
     return (
+        <>
         <section className='textbar_sec'>
             <input type = 'text' className = 'text_bar'/>
             <button className = 'textbar_button'> <img className = 'textbarbutton_image' alt = 'sendbutton' src = {sendbutton}/></button>
         </section>
+        <div className = 'text_cover'>
+        
+        </div>
+        </>
 
     )
 }
