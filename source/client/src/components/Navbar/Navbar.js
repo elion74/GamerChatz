68a6cbf34c205177a18d828ca092f518ee92ec7a
Elion Shahini <elion.shahini74@gmail.com>
2021-07-23 15:08:20 +0200

    hamburger menu completly finshed. Added Chats and Group component to wrap content.

--- source/client/src/components/Navbar/Navbar.js
+++ source/client/src/components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 
 import Search from '../../components/Search/Search.js';
+import Chat from '../Chat/Chat.js';
+import Group from '../Group/Group.js';
 
 import './Navbar.css';
 
 import gamerchatzLogo from '../../assets/gamerchatzLogo.png';
+
+import notificationLogo from '../../assets/notification.png';
+import img from '../../assets/defaultprofilepic.jpg';
+
+
 
 export default function Navbar() {
 
@@ -26,6 +33,48 @@ export default function Navbar() {
                     <div className = 'hamburger_line1'></div>
                     <div className = 'hamburger_line2'></div>
                 </div>
+
+                { open ?
+                <>
+                <h2 className = 'wrapchats_headline'>Your Chats</h2>
+                <section className = 'wrap_chats'>
+                 <Chat /> 
+                 <Chat /> 
+                 <Chat /> 
+                 <Chat /> 
+                 <Chat /> 
+                 <Chat /> 
+                </section>
+                </>
+                : ''}
+
+                { open ?
+                <>
+                <h2 className = 'wrapchats_headline'>Your Groups</h2>
+                <section className = 'wrap_chats'>
+                 <Group/> 
+                 <Group /> 
+                 <Group /> 
+                 <Group /> 
+                 <Group /> 
+                 <Group /> 
+                </section>
+            
+                <h2 className = 'wrapchats_headline'>Source Code</h2>
+                
+                <div style = {{width:"80%", margin:"0 auto", borderTop:" #787b7d solid 1px", marginTop:"65px", marginBottom:'60px'}}></div>
+
+                <section className = 'profile_sec'>
+                    <div className = 'profile_container'>
+                        <img className ='notification' src = {notificationLogo} alt='notification'/>
+                        <img  className ='yourprofile_pic' src ={img} alt='yourprofilepic'/>
+                    </div>   
+                </section>
+                </>
+                : ''}
+
+
+
             </div>
 
           <nav className='nav_links'>
