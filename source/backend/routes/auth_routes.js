f83c658237faaebc8becdf4a6118829d57f79ca9
Elion Shahini <elion.shahini74@gmail.com>
2021-09-13 18:11:23 +0200

    Auth system setup, but without github and discord

--- source/backend/routes/auth_routes.js
+++ source/backend/routes/auth_routes.js
@@ -1,28 +1,29 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-
+// google auth  
 router.get('/google', 
     passport.authenticate('google', { scope: [ 'email', 'profile' ] })
 );
 
-
 router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/auth/google/failure' }),
+  passport.authenticate('google', { failureRedirect: '/auth/failure' }),
   function(req, res) {
-    // Successful authentication, redirect home.
+    // Successful authentication
     console.log('users auth success');
     res.status(308).redirect('/protected');
 });
 
-router.get('/google/logout', (req, res) => {
+
+router.get('/logout', (req, res) => {
+    console.log('user logged out')
     req.logout(); // logout() will clear req.user and sessions
     req.session.destroy(); // destroys the session
     res.status(204).send('Goodbye!');
 });
 
 // das maybe zusammen, für alle failed auth dienste
-router.get('/google/failure', (req, res) => {
+router.get('/failure', (req, res) => {
     res.status(403).send('Failed to authenticate..');
 });
 
